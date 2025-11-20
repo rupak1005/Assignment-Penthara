11ebdbb870e0c20199df14189678b6a3524703fd
Frontend Trainee <trainee@example.com>
2025-11-20 22:35:47 +0530

    Update CORS configuration to include additional production URLs for enhanced access control.

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -9,7 +9,7 @@ const app = express();
 const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:3000",
-  "https://assignment-penthara.vercel.app/",   // <--- ADD THIS
+  env.corsOrigin,
 
 ];
 
